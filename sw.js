4d10018152a81435921ade3e988c408312b170b9
woogiiin-maker <woogiii.n@gmail.com>
2026-09-12 16:12:12 +0900

    insur_v0.5.1
    보험 두번째

---
+++ sw.js
@@ -0,0 +1,12 @@
+﻿const CACHE_NAME = 'family-insurance-cache-v1';
+self.addEventListener('install', (e) => {
+ self.skipWaiting();
+});
+self.addEventListener('activate', (e) => {
+ e.waitUntil(self.clients.claim());
+});
+self.addEventListener('fetch', (e) => {
+ e.respondWith(
+ fetch(e.request).catch(() => caches.match(e.request))
+ );
+});
