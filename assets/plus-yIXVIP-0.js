bf15a877ad302f881ac5dddf5561e7758f52006d
woogiiin-maker <woogiii.n@gmail.com>
2026-09-13 14:14:25 +0900

    Delete assets/plus-yIXVIP-0.js

--- assets/plus-yIXVIP-0.js
+++
@@ -1,6 +0,0 @@
-import{c as a}from"./index-hILOHhsm.js";/**
- * @license lucide-react v1.45.0 - ISC
- *
- * This source code is licensed under the ISC license.
- * See the LICENSE file in the root directory of this source tree.
- */const e={name:"plus",size:24,node:[["path",{d:"M5 12h14",key:"1ays0h"}],["path",{d:"M12 5v14",key:"s699le"}]]};e.node;const s=a(e);export{s as P};
