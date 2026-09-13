379e460ccd92998191894b8afbfd081efc7b900e
woogiiin-maker <woogiii.n@gmail.com>
2026-09-13 14:14:42 +0900

    Delete assets directory

--- assets/check-B-QJPtrY.js
+++
@@ -1,6 +0,0 @@
-import{c as e}from"./index-4qqT3rQ6.js";/**
- * @license lucide-react v1.45.0 - ISC
- *
- * This source code is licensed under the ISC license.
- * See the LICENSE file in the root directory of this source tree.
- */const c={name:"check",size:24,node:[["path",{d:"M20 6 9 17l-5-5",key:"1gmf2c"}]]};c.node;const a=e(c);export{a as C};
